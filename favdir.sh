abc378e733005dfd300659f5b8ddd4ed5f051130
Z1Z1R07 <z1z1r07@gmail.com>
2014-10-09 23:38:25 +0900

    Fix basename bug

--- favdir.sh
+++ favdir.sh
@@ -114,7 +114,7 @@ function _favdir_show()
   # show help about show function
   [ "$1" = "-h" ] || [ "$1" = "--help" ] && { _favdir_usage 'show'; return 0; }
   # if listfile does not exist, output error message and exit
-  [ -f $favdir_list ] || { echo "$(basename $bookmarklist): no exist"; return 1; }
+  [ -f $favdir_list ] || { echo "$(basename $favdir_list): no exist"; return 1; }
 
   local opt
   local -i i 
