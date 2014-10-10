6d08814a7bc39fbbb64a2a34380e0b6de239cebd
Z1Z1R07 <z1z1r07@gmail.com>
2014-10-10 17:02:12 +0900

    More simplify

--- install.sh
+++ install.sh
@@ -1,31 +1,26 @@
-#!/usr/bin/env bash
-
-#INSTALL_DIR=~/.local/bin
-#INSTALL_FILE=favdir.sh
-#FAVDIR=~/.favdir
+#!/bin/bash
 
 cd "$(dirname "${BASH_SOURCE}")"
-
 git pull origin master
 
-doInstall()
+function install()
 {
-	local INSTALL_DIR="$HOME/.local/bin"
-	mkdir -p $INSTALL_DIR
-	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
-		--exclude "Makefile" --exclude "README.md" -avh --no-perms . $INSTALL_DIR
-	echo "If you did not write the following, and write it to your shell rc file"
-	echo "Add 'source ~/.local/bin/favdir.sh' to ~/.bashrc or ~/.zshrc"
-	echo ", and  re-start your shell..."
+    local INSTALL_DIR="$HOME/.local/bin"
+    mkdir -p $INSTALL_DIR
+    cp favdir.sh "$INSTALL_DIR"
+
+    local msg=$(
+    cat <<-EOF
+    If you did not write the following, and write it to your shell rc file
+    Add 'source ~/.local/bin/favdir.sh' to ~/.bashrc or ~/.zshrc
+    , and  re-start your shell...
+    EOF
+    );
+    echo "$msg"
 }
 
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doInstall;
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo ""
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doInstall;
-	fi
+read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    install;
 fi
-unset doInstall
